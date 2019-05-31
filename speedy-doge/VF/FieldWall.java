45d2791348269224cf4e2487b9f64044046ed1db
theZebraBatman <fgportman00@gmail.com>
2019-05-31 13:04:35 -0400

    fixed activation issue in FieldWall

--- speedy-doge/VF/FieldWall.java
+++ speedy-doge/VF/FieldWall.java
@@ -8,14 +8,16 @@ public class FieldWall implements Obstical {
     private boolean isActive;
 
     public Vector2 interact(Pose point, Vector2 vector) {
-        if(point.x > 72 - UniversalConstants.EFFECTIVE_ROBOT_RADIUS && vector.x > 0)
-            vector.x = 0;
-        if(point.x < -72 + UniversalConstants.EFFECTIVE_ROBOT_RADIUS && vector.x < 0)
-            vector.x = 0;
-        if(point.y > 72 - UniversalConstants.EFFECTIVE_ROBOT_RADIUS && vector.y > 0)
-            vector.y = 0;
-        if(point.y < -72 + UniversalConstants.EFFECTIVE_ROBOT_RADIUS && vector.y < 0)
-            vector.y = 0;
+        if (isActive) {
+            if (point.x > 72 - UniversalConstants.EFFECTIVE_ROBOT_RADIUS && vector.x > 0)
+                vector.x = 0;
+            if (point.x < -72 + UniversalConstants.EFFECTIVE_ROBOT_RADIUS && vector.x < 0)
+                vector.x = 0;
+            if (point.y > 72 - UniversalConstants.EFFECTIVE_ROBOT_RADIUS && vector.y > 0)
+                vector.y = 0;
+            if (point.y < -72 + UniversalConstants.EFFECTIVE_ROBOT_RADIUS && vector.y < 0)
+                vector.y = 0;
+        }
         return vector;
     }
 
