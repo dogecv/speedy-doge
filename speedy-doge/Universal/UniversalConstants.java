53395f9b0638d41e53666d2c29885b873aae8761
theZebraBatman <fgportman00@gmail.com>
2019-05-31 10:14:49 -0400

    Added EFFECTIVE_ROBOT_RADIUS to UniversalConstants

--- speedy-doge/Universal/UniversalConstants.java
+++ speedy-doge/Universal/UniversalConstants.java
@@ -1,4 +1,7 @@
 package Universal;
 
 public class UniversalConstants {
+
+    //how big we treat the robot when avoiding obsticals
+    public static final double EFFECTIVE_ROBOT_RADIUS = 9 * Math.sqrt(2) + 2;
 }
